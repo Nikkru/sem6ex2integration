5dcf61ee86d455d7c45f82ff1e33820d447944e1
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-02-03 12:43:17 +0300

    init

---
+++ src/main/java/com/example/sem6ex2/Sem6ex2Application.java
@@ -0,0 +1,13 @@
+package com.example.sem6ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem6ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem6ex2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sem6ex2/model/Book.java
@@ -0,0 +1,4 @@
+package com.example.sem6ex2.model;
+
+public class Book {
+}
